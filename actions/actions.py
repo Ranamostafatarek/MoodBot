5e8972042ed96fe1d6c8266b2a76d88859e610dd
ranamostafatarek <ranam.tarek@gmail.com>
2022-06-16 16:17:57 +0200

    Latest

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,36 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionReceiveSentence(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_receive_sentence"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        text = tracker.latest_message['text']
+        dispatcher.utter_message(text=f"I'll remember your sentence!")
+        return [SlotSet("sentence", text)]
+
+
+class ActionSaySentence(Action):
+
+    def name(self) -> Text:
+        return "action_repeat_sentence"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        sentence = tracker.get_slot("sentence").upper()
+        if not sentence:
+            dispatcher.utter_message(text="I don't know what to repeat")
+        else:
+            dispatcher.utter_message(text=f"I want to tell you that {sentence}!")
+        return []
